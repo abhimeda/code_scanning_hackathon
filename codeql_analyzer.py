bb39fc4ded72b758791bb693a359b3dee315de93
Daniel Su <danielsu@amd.com>
2024-06-12 16:24:30 -0400

    add preview lines to output json

--- codeql_analyzer.py
+++ codeql_analyzer.py
@@ -3,16 +3,15 @@ import subprocess
 from app import app
 
 
-def analyze_codeql_db(database_name: str) -> None:
+def analyze_codeql_db(database_name: str, analyze_file: str):
     """Runs the CodeQL analyzer on behalf of the user and retrieves the outputted
     sarif file.
     :param name:
     :return:
     """
     # Run the CodeQL command to analyze the repository
-    result = subprocess.run(
-        ["codeql", "database", "analyze", database_name, "--format=sarif-latest", "--output=results.sarif"],
-        capture_output=True, text=True)
+    result = subprocess.run(["codeql", "database", "analyze", database_name,
+                            "--format=sarif-latest", f"--output={analyze_file}"], capture_output=True, text=True)
     if result.returncode == 0:
         print("CodeQL analysis completed successfully")
     else:
@@ -20,28 +19,21 @@ def analyze_codeql_db(database_name: str) -> None:
         print(result.stderr)
 
 
-#  Written by Copilot
-def parse_sarif_file() -> None:
+def parse_sarif_file(analyze_file: str):
     """Parses the SARIF file and extracts the vulnerabilities contained in the results array.
     :return:
     """
-    with open("results.sarif") as f:
+    with open(analyze_file) as f:
         sarif_data = json.load(f)
-
-    data = sarif_data["runs"][0]
-    results = data["results"]
-    # Check if results exist and is not empty
-    if results:
-        with open("output.json", "w") as f:
-            json.dump(results, f, indent=4)
-    else:
-        print("No vulnerabilities found in SARIF file.")
+        results = sarif_data["runs"][0]["results"]
+        if results:
+            return results
+        else:
+            print("No vulnerabilities found in SARIF file.")
 
 
-def analyze_json_results():
+def analyze_json_results(results):
     ls_locations = []
-    with open("output.json", "r") as f:
-        results = json.load(f)
     for result in results:
         # Access dictionary elements
         vulnerability = result["message"]["text"]
@@ -50,11 +42,12 @@ def analyze_json_results():
             # Access location details
             file_path = location["physicalLocation"]["artifactLocation"]["uri"]
             line = location["physicalLocation"]["region"]["startLine"]
-            ls_locations.append({"file_path": file_path, "line": line, "message": vulnerability})
+            ls_locations.append(
+                {"file_path": file_path, "line": line, "message": vulnerability})
     return ls_locations
 
 
-def get_code_at_line(file_path: str, line_number: int) -> str:
+def get_code_at_line(file_path: str, line_number: int):
     """Reads the file and retrieves the code at a specific line number.
     :param file_path: The path to the file.
     :param line_number: The line number.
@@ -63,20 +56,22 @@ def get_code_at_line(file_path: str, line_number: int) -> str:
     with open(file_path) as f:
         lines = f.readlines()
         if line_number <= len(lines):
-            code = lines[line_number - 1].strip()
+            code = {
+                "single": lines[line_number - 1].strip(),
+                "preview": lines[max(0, line_number - 4):min(len(lines), line_number + 3)]
+            }
             return code
         else:
             print("Line number exceeds the total number of lines in the file.")
 
-    return ""
 
-
-def search_git_history(file_path:str, keyword: str) -> dict[str, str] | str:
+def search_git_history(file_path: str, keyword: str):
     """Searches the git history for a specific keyword using the 'git log -S' command.
     :param keyword: The keyword to search for.
     :return: The output of the git log command.
     """
-    result = subprocess.run(["git", "log", "-S", keyword, "--", file_path], capture_output=True, text=True)
+    result = subprocess.run(
+        ["git", "log", "-S", keyword, "--", file_path], capture_output=True, text=True)
     if result.returncode == 0:
         # Extract the sha, author, and time from the git log output
         git_log_output = result.stdout
@@ -104,18 +99,23 @@ def search_git_history(file_path:str, keyword: str) -> dict[str, str] | str:
 
 if __name__ == "__main__":
 
-    authors = []
-    analyze_codeql_db("database")
-    parse_sarif_file()
-    vulnerable_locations = analyze_json_results()
-    for location in vulnerable_locations:
-        code_snippet = get_code_at_line(location["file_path"], location["line"])
-        author = search_git_history(location["file_path"], code_snippet)
-        author["file_path"] = location["file_path"]
-        author["line"] = location["line"]
-        author["message"] = location["message"]
-        authors.append(author)
-    with open("authors.json", "w") as f:
-        json.dump(authors, f, indent=4)
+    database_name = "database"
+    analyze_file = "analyze.sarif"
+    output_file = "authors.json"
+    output = []
 
+    analyze_codeql_db(database_name, analyze_file)
+    results = parse_sarif_file(analyze_file)
+    vulnerable_locations = analyze_json_results(results)
+    for location in vulnerable_locations:
+        code = get_code_at_line(location["file_path"], location["line"])
+        vuln = search_git_history(location["file_path"], code["single"])
+        vuln["file_path"] = location["file_path"]
+        vuln["line"] = location["line"]
+        vuln["message"] = location["message"]
+        vuln["preview"] = code["preview"]
+        output.append(vuln)
+
+    with open(output_file, "w") as f:
+        json.dump(output, f, indent=4)
     app.run()
