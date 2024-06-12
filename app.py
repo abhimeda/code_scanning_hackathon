cf25cd3e8f225adb86234a3c5baaae6972ae16f0
Mustaballer <mus2003.abdul@gmai.com>
2024-06-12 14:48:46 -0400

    load json and add more css style

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
+import json
+
 from flask import Flask, render_template
+
+import codeql_analyzer
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # TEMPORARY DATA
+    # get data 
+    file_path = 'authors.json'
+
+    # Open the file and load the data
+    posts = []
+    with open(file_path, 'r') as file:
+        posts = json.load(file)
     
-    posts = [
-        {"author": "Author1", "date": "2023-04-01", "lines": "123 456 789", "suggestions": "Improve readability"},
-        {"author": "Author2", "date": "2023-04-02", "lines": "234 567 890", "suggestions": "Optimize performance"},
-        {"author": "Author3", "date": "2023-04-03", "lines": "345 678 901", "suggestions": "Refactor for clarity"},
-        {"author": "Author4", "date": "2023-04-04", "lines": "456 789 012", "suggestions": "Update documentation"}
-    ]
     return render_template('index.html', posts=posts)
