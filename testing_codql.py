7a449da3b28159b45b36abe6bf62417ba5240994
Meda, Abhinav <Abhinav.Meda@amd.com>
2024-06-12 13:28:55 -0400

    adding all changes

--- testing_codql.py
+++ testing_codql.py
@@ -1,11 +1,22 @@
-import os
+import json
+import json
 
 
-# Vulnerability: Command Injection
-def command_injection(user_input):
-    os.system(user_input)
+def main():
+    sarif_file = "./output.sarif"
 
+    with open(sarif_file, "r") as file:
+        sarif_data = json.load(file)
+
+    data = sarif_data["runs"][0]
+    results = data["results"]
+
+
+    # for every dictionary in results, there is a key called locations that specifies where
+    # the vulnerability exists.
+    
+    with open("results.json", "w") as file:
+        json.dump(results, file, indent=5)
 
 if __name__ == "__main__":
-    user_input = input("Enter a command: ")
-    command_injection(user_input)
+    main()
